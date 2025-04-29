a785c99c14f7574fe3ac278a5960fb2f38e008f3
sks2k6 <108617100+sks2k6@users.noreply.github.com>
2025-04-29 23:32:59 +0530

    Update and rename index.html to index.js

---
+++ index.js
@@ -0,0 +1,37 @@
+import makeWASocket, { DisconnectReason, useMultiFileAuthState } from 'baileys'
+import { Boom } from '@hapi/boom'
+
+async function connectToWhatsApp () {
+    const { state, saveCreds } = await useMultiFileAuthState('auth_info_baileys')
+    const sock = makeWASocket({
+        // can provide additional config here
+        auth: state,
+        printQRInTerminal: true
+    })
+    sock.ev.on('connection.update', (update) => {
+        const { connection, lastDisconnect } = update
+        if(connection === 'close') {
+            const shouldReconnect = (lastDisconnect.error as Boom)?.output?.statusCode !== DisconnectReason.loggedOut
+            console.log('connection closed due to ', lastDisconnect.error, ', reconnecting ', shouldReconnect)
+            // reconnect if not logged out
+            if(shouldReconnect) {
+                connectToWhatsApp()
+            }
+        } else if(connection === 'open') {
+            console.log('opened connection')
+        }
+    })
+    sock.ev.on('messages.upsert', event => {
+        for (const m of event.messages) {
+            console.log(JSON.stringify(m, undefined, 2))
+
+            console.log('replying to', m.key.remoteJid)
+            await sock.sendMessage(m.key.remoteJid!, { text: 'Hello Word' })
+        }
+    })
+
+    // to storage creds (session info) when it updates
+    sock.ev.on('creds.update', saveCreds)
+}
+// run in main file
+connectToWhatsApp()
